dd6a87c75911d5ebe28dcb0f4ff21cf548517a23
ngvuong <vminguyen@ucdavis.edu>
2021-09-05 04:17:16 -0700

    Add menu, contact tabs

---
+++ src/menu.js
@@ -0,0 +1,13 @@
+export function addMenu() {
+  const content = document.querySelector("#content");
+  const title = document.createElement("h1");
+  const foodSubtitle = document.createElement("h2");
+  const menu = document.createElement("div");
+  const item1 = document.createElement("div");
+  const item2 = document.createElement("div");
+  const item3 = document.createElement("div");
+  const item4 = document.createElement("div");
+  const drinkSubtitle = document.createElement("h2");
+  const drink1 = document.createElement("div");
+  const drink1 = document.createElement("div");
+}
