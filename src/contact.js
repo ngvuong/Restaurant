28f0aa08ef1b84dba7970588b5eb1f857beee341
ngvuong <vminguyen@ucdavis.edu>
2021-09-05 13:52:30 -0700

    Update restaurant content

---
+++ src/contact.js
@@ -0,0 +1,4 @@
+export function addContact() {
+  const contactInfo = document.createElement("div");
+  const hours = document.createElement("div");
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import { addContent } from "./content.js";
+import { addMenu } from "./menu.js";
+import { addContacts } from "./contacts.js";
+
 addContent();
+
+document.addEventListener("click", () => {});
