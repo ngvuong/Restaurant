1f7525e734efc22ebb7a160147a313b69541bc52
ngvuong <vminguyen@ucdavis.edu>
2021-09-06 21:32:54 -0700

    Update restaurant nav

--- src/content.js
+++ src/content.js
@@ -1,5 +1,6 @@
 export function addContent() {
   const content = document.querySelector("#content");
+  const main = document.createElement("main");
   const headline = document.createElement("h1");
   const message = document.createElement("p");
 
@@ -7,7 +8,10 @@ export function addContent() {
   message.textContent = `Welcome! Feel at home and have a lovely meal at Grandma's Homemade!
      We have all of your favorite Grandma's signature Vietnamese cuisines and drinks.`;
   message.classList.add("message");
+  main.classList.add("home");
+  main.append(headline, message);
+
   window.addEventListener("load", () => {
-    content.append(headline, message);
+    content.appendChild(main);
   });
 }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ addContent();
 
 const content = document.querySelector("#content");
 const navBar = document.createElement("nav");
+const home = document.createElement("button");
+const menu = document.createElement("button");
+const contact = document.createElement("button");
 navBar.classList.add("nav");
-document.body.prepend(navBar);
+navBar.document.body.prepend(navBar);
 
 document.addEventListener("click", () => {});
