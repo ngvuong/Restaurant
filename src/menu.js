3b1da88533a8e4a8a11b525fe1b73fc10728c76d
ngvuong <vminguyen@ucdavis.edu>
2021-09-07 01:08:54 -0700

    Update restaurant menu items

--- src/menu.js
+++ src/menu.js
@@ -2,21 +2,44 @@ export function addMenu() {
   const content = document.querySelector("#content");
 
   const main = document.createElement("main");
-  const title = document.createElement("h1");
   const food = document.createElement("section");
-  const foodSubtitle = document.createElement("h2");
-  const menu = document.createElement("div");
-  const item1 = document.createElement("div");
-  const item2 = document.createElement("div");
-  const item3 = document.createElement("div");
-  const item4 = document.createElement("div");
+  const foodHeading = document.createElement("h2");
+  const foodItems = document.createElement("ol");
+  const dish1 = document.createElement("li");
+  const dish2 = document.createElement("div");
+  const dish3 = document.createElement("div");
+  const dish4 = document.createElement("div");
   const drinks = document.createElement("section");
-  const drinkSubtitle = document.createElement("h2");
+  const drinksHeading = document.createElement("h2");
+  const drinkItems = document.createElement("ol");
   const drink1 = document.createElement("div");
   const drink2 = document.createElement("div");
 
-  main.classList.add("menu-page");
+  food.classList.add("menu-category");
+  foodHeading.textContent = "Dishes";
+  dish1.innerHTML = `<em>Phở Bò</em> <br />
+    - Beef Noodle soup <br/>
+    This is Grandma's favorite dish.`;
 
+  dish2.innerHTML = `<em>Phở Gà</em> <br />
+    - Beef Noodle soup <br/>
+    This is Grandma's favorite dish.`;
+
+  dish3.innerHTML = `<em>Bánh Mì</em> <br />
+    - Vietnamese Sandwich <br/>
+    This is Grandma's favorite dish.`;
+
+  dish4.innerHTML = `<em></em></em> <br />
+    - Beef Noodle soup <br/>
+    This is Grandma's favorite dish.`;
+
+  foodItems.append(dish1, dish2, dish3, dish4);
+
+  drinks.classList.add("menu-category");
+  drinksHeading.textContent = "Drinks";
+  food.append(foodHeading, foodItems);
+  drinks.append(drinksHeading, drinkItems);
+  main.classList.add("menu-page");
   main.append(food, drinks);
   content.append(main);
 }
