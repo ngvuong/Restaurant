850bf26e046c82681839951ac89040046f8a58c0
ngvuong <vminguyen@ucdavis.edu>
2021-09-06 23:04:24 -0700

    Update restaurant display content

--- src/home.js
+++ src/home.js
@@ -1,6 +1,6 @@
-export function addContent() {
+export function addHome() {
   const content = document.querySelector("#content");
-  const homeBtn = document.querySelector(".nav").firstElementChild();
+  const homeBtn = document.querySelector(".btn");
   const main = document.createElement("main");
   const headline = document.createElement("h1");
   const message = document.createElement("p");
@@ -9,8 +9,10 @@ export function addContent() {
   message.textContent = `Welcome! Feel at home and have a lovely meal at Grandma's Homemade!
      We have all of your favorite Grandma's signature Vietnamese cuisines and drinks.`;
   message.classList.add("message");
-  main.classList.add("home");
+  main.classList.add("home-page");
   main.append(headline, message);
+
+  homeBtn.classList.add("active");
 
   window.addEventListener("load", () => {
     content.appendChild(main);
--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
-import { addContent } from "./content.js";
+import { addHome } from "./home.js";
 import { addMenu } from "./menu.js";
 import { addContact } from "./contact.js";
 
-addContent();
+(function contentDisplay() {
+  const content = document.querySelector("#content");
+  const navBar = document.createElement("nav");
+  const home = document.createElement("button");
+  const menu = document.createElement("button");
+  const contact = document.createElement("button");
 
-const content = document.querySelector("#content");
-const navBar = document.createElement("nav");
-const home = document.createElement("button");
-const menu = document.createElement("button");
-const contact = document.createElement("button");
+  home.textContent = "Home";
+  menu.textContent = "Menu";
+  contact.textContent = "Contact";
 
-home.textContent = "Home";
-menu.textContent = "Menu";
-contact.textContent = "Contact";
+  home.classList.add("btn", "home");
+  menu.classList.add("btn", "menu");
+  contact.classList.add("btn", "contact");
 
-home.classList.add("active");
-navBar.classList.add("nav");
-navBar.append(home, menu, contact);
-document.body.prepend(navBar);
+  navBar.classList.add("nav");
+  navBar.append(home, menu, contact);
+  document.body.prepend(navBar);
+  addHome();
 
-document.addEventListener("click", () => {});
+  buttons = document.querySelectorAll(".btn");
+
+  function addContent() {}
+
+  buttons.forEach((btn) => {
+    console.log(this);
+    btn.addEventListener("click", addContent);
+  });
+})();
