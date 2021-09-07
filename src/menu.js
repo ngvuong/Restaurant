83c147999a30b719d7417866f8dd0128de402a0d
ngvuong <vminguyen@ucdavis.edu>
2021-09-07 00:06:48 -0700

    Update restaurant menu

--- src/menu.js
+++ src/menu.js
@@ -1,13 +1,22 @@
 export function addMenu() {
   const content = document.querySelector("#content");
+
+  const main = document.createElement("main");
   const title = document.createElement("h1");
+  const food = document.createElement("section");
   const foodSubtitle = document.createElement("h2");
   const menu = document.createElement("div");
   const item1 = document.createElement("div");
   const item2 = document.createElement("div");
   const item3 = document.createElement("div");
   const item4 = document.createElement("div");
+  const drinks = document.createElement("section");
   const drinkSubtitle = document.createElement("h2");
   const drink1 = document.createElement("div");
   const drink2 = document.createElement("div");
+
+  main.classList.add("menu-page");
+
+  main.append(food, drinks);
+  content.append(main);
 }
