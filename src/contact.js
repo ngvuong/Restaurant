c85dbbf6fc8456f3dda514704fb94a1379af8810
ngvuong <vminguyen@ucdavis.edu>
2021-09-07 22:13:10 -0700

    Update restaurant contact page

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,16 @@
 export function addContact() {
+  const content = document.querySelector("#content");
+  const main = document.createElement("main");
   const contactInfo = document.createElement("div");
   const hours = document.createElement("div");
+
+  contactInfo.innerHTML = `<p>Phone: 777-545-6000</p>
+    <p>Email: Gma55@gmail.com</p>
+    55 G Street <br/>
+    Sumware, MA 54545
+    <p>Open everyday 10 A.M - 8 P.M</p>`;
+
+  main.classList.add("contact-page");
+  main.append(contactInfo);
+  content.append(main);
 }
